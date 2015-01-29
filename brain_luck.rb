21605c3e94c4098d37d53739b89ecd5613b82d0b
Ian Rash <redcodefinal@gmail.com>
2015-01-29 08:12:55 -0800

    Update brain_luck.rb

--- brain_luck.rb
+++ brain_luck.rb
@@ -2,26 +2,26 @@ def brain_luck(code, input)
   output = ''
   data = [0]
   pointer = 0
-  code.each_char do |instruction| 
+  current_instruction = 0
+  while current_instruction < code.length
+    instruction = code[current_instruction]
     #interpret instruction
     if instruction == '>'
       #increment the pointer
       pointer += 1
-      if (pointer + 1) > data.length
-        data << 0
-      end
+      data << 0 if (pointer + 1) > data.length
     elsif instruction == '<'
       pointer -= 1
+      if pointer < 0
+        pointer = 0
+        data.unshift 0
+      end
     elsif instruction == '+'
       data[pointer] += 1
-      if data[pointer] > 255
-        data[pointer] = 0
-      end
+      data[pointer] = 0 if data[pointer] > 255
     elsif instruction == '-'
       data[pointer] -= 1
-      if data[pointer] < 0
-        data[pointer] = 255
-      end
+      data[pointer] = 255 if data[pointer] < 0
     elsif instruction == '.'
       output << data[pointer].chr
     elsif instruction == ','
